93afa844f9b5ab7d3c3d3136e1c00dfe64fb9e19
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-23 19:18:20 -0500

    added contact form

--- frontend/src/views/contact.js
+++ frontend/src/views/contact.js
@@ -1,9 +1,32 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 const Contact = () => {
     return(
-        <div>
-            Contact Form
+        <div className={'contact-main'}>
+            <div className="contact-content">
+            <div>Were here to answer your questions</div>
+            <TextField
+              placeholder="Email Address"
+              label="What's your e-mail address?"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              placeholder="Type your question here"
+              label="What's your question or issue?"
+              margin="normal"
+              fullWidth
+            />
+
+            <Button
+              color="primary"
+              variant="contained"
+            >Send Email</Button>
+            </div>
+
         </div>
     )
 }
