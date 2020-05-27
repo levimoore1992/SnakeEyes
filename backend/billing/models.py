830f07c1d159de08b322a00c8028781be525e93c
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-27 07:19:09 -0500

    added methods to the bet model

--- backend/billing/models.py
+++ backend/billing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import UserProfile
+from datetime import datetime
 
 
 class Bet(models.Model):
@@ -9,4 +10,32 @@ class Bet(models.Model):
     die_2 = models.IntegerField()
     roll = models.IntegerField()
     wagered = models.BigIntegerField()
+    payout = models.FloatField()
+    net = models.FloatField()
 
+    @property
+    def is_winner(self):
+        if self.guess == self.roll:
+            return True
+        return False
+
+    @property
+    def determine_payout(self):
+
+        if self.is_winner:
+            return self.payout
+        return 1.0
+
+    @property
+    def calculate_net(self):
+        if is_winner:
+            return int(self.wagered * self.payout)
+
+        return -self.wagered
+
+    def save_and_update_user(self, user):
+        self.save()
+        user.coins += self.net
+        user.last_bet_on = datetime.now()
+
+        return user.save()
