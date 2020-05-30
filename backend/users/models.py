90fe065975e029351e38c9e22a8958f3872cfe53
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-30 17:19:45 -0500

    added some fields to the model

--- backend/users/models.py
+++ backend/users/models.py
@@ -9,5 +9,15 @@ class UserProfile(models.Model):
     is_active = models.BooleanField(default=True)
     sign_in_count = models.IntegerField()
 
+    #Billing
+    name = models.CharField(max_length=128)
+    payment_id = models.CharField(max_length=128)
+    cancelled_subscription = models.DateTimeField(auto_now=True)
+    previous_plan = models.CharField(max_length=45)
+
+    #Bet
+    coins = models.BigIntegerField()
+    last_bet_on = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.user.username
